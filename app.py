a6f9c462c074691abfbc5cb36837e27cf2d80b18
Felipe Lima notebook <felipy0439@gmail.com>
2024-10-05 16:30:40 -0300

    rebase: error correction

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-from pages.home import home
-home()
+import streamlit as st
+from css import css
+
+
+st.markdown(css, unsafe_allow_html=True)
+st.markdown(
+    '<h1>Bem-vindo ao CLOROFILA TECH</h1>',
+    unsafe_allow_html=True,
+)
+st.write("Somos um sistema de gerenciamento de floriculturas.")
+st.write("Use a barra lateral para navegar")
