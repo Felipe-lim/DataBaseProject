1a3e4857db90151d542c51f613274dc766e2dbd2
Victornw <victormarques.dev@gmail.com>
2024-10-06 14:28:08 -0300

    UPDATE - Nome das paginas

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 from pags.Start.start import display_start
-from pags.Manage.manager import display_manager
+from pags.users.manager import display_manager
 from pags.reports.report import display_report
 from pags.orders.Compra import display_compras
 
 
 def display_pages():
    st.sidebar.title("Navegação")
-   page = st.sidebar.radio("Ir para:", ["Start", "Manage Users","Relatórios", "Orders"])
+   page = st.sidebar.radio("Ir para:", ["Início", "Gerenciar Usuários","Relatórios", "Pedidos"])
 
    match page:
-      case "Start":
+      case "Início":
          display_start()
-      case "Manage Users":
+      case "Gerenciar Usuários":
          display_manager()
       case "Relatórios":
          display_report()
-      case "Orders":
+      case "Pedidos":
          display_compras()
 
 def main():
